bfaea8059a94b35be96d6e73b4e1814d15897f7f
aqib.ahmed <aqib.ahmed@samarth.ac.in>
2023-07-09 01:07:16 +0530

    Added Data Type

---
+++ data_type.cpp
@@ -0,0 +1,13 @@
+int dataTypes(string type) {
+	// Write your code here
+	if(type=="Long"){
+		return 8;
+	}else if(type=="Integer"){
+		return 4;
+	}else if(type=="Float"){
+		return 4;
+	}else if(type=="Double"){
+		return 8;
+	}
+	return 1;
+}
